4fff1c95a0ceb054c44dced9fe9865507e3281c1
ASHU-KUMAR-work <24110cn047@glbitm.ac.in>
2026-06-01 11:43:14 +0530

    Update stock_buy-sell.cpp

--- Week_1/Day_1/stock_buy-sell.cpp
+++ Week_1/Day_1/stock_buy-sell.cpp
@@ -1,1 +1,16 @@
-
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int mp = 0;
+        int min = INT_MAX;
+        for(int i:prices){
+            if(i<min){
+                min = i;
+            }
+            else{
+                mp = max(mp,i-min);
+            }
+        }
+        return mp;
+    }
+};
